39d81975b1b58dde7d7120ad738da05c089dbb09
Ghuffran <ghuffran.dev@gmail.com>
2023-10-04 16:03:57 -0400

    name added in topbar

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 import { Avatar, Dropdown, Menu } from "antd";
 import { useNavigate } from "react-router-dom";
 import { ToggleButton } from "./styled";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "src/store/slices/authSlice/apis";
+import { getLogedInUser } from "src/store/slices/authSlice/selectors";
 
 const Topbar = ({ setCollapsed, collapsed }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector(getLogedInUser);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -13,10 +18,17 @@ const Topbar = ({ setCollapsed, collapsed }) => {
     navigate("/login");
   };
 
+  useEffect(() => {
+    dispatch(loginUser());
+  }, []);
+
   const menu = (
     <Menu>
-      <Menu.Item key="1">settings</Menu.Item>
-      <Menu.Item key="2" onClick={() => handleLogout()}>
+      <Menu.Item disabled key="welcome">
+        Welcome {user.first_name} {user.last_name}
+      </Menu.Item>
+      <Menu.Item key="settings">Settings</Menu.Item>
+      <Menu.Item key="logout" onClick={() => handleLogout()}>
         Log out
       </Menu.Item>
     </Menu>
@@ -35,11 +47,11 @@ const Topbar = ({ setCollapsed, collapsed }) => {
       <ToggleButton onClick={() => setCollapsed((prev) => !prev)}>
         {collapsed ? <FaAngleRight size="20px" /> : <FaAngleLeft size="20px" />}
       </ToggleButton>
-      <Dropdown overlay={menu}>
+      <Dropdown overlay={menu} placement="bottomRight">
         <Avatar
           size={"large"}
           src="https://joesch.moe/api/v1/random"
-          style={{ border: "1px solid lightgray" }}
+          style={{ border: "1px solid lightgray", cursor: "pointer" }}
         />
       </Dropdown>
     </div>
