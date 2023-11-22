c509f58dbcd0b46e79fe5a11b71f81f2e74e3cbc
saadabid2223 <saadabid03@gmail.com>
2023-11-23 02:50:30 +0500

    sign up form changes

--- src/features/SignUp/index.js
+++ src/features/SignUp/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Form, Input, Button, Avatar, message } from "antd";
+import { Form, Input, Button, Avatar, message, Flex } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { FormContainer, Title, Wrapper, FieldGroup } from "./styled";
+import { FormContainer, Title, Wrapper } from "./styled";
 import { registerUser } from "src/store/slices/authSlice/apis";
 
 const SignUpForm = () => {
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -18,6 +19,7 @@ const SignUpForm = () => {
                     password: values.password,
                     first_name: values.first_name,
                     last_name: values.last_name,
+                    phoneNumber: values.phoneNumber,
                 })
             ).unwrap();
 
@@ -38,43 +40,72 @@ const SignUpForm = () => {
                 <Avatar size={64} icon={<UserOutlined />} style={{ marginBottom: 24 }} />
                 <Title>Sign Up</Title>
                 <Form name="register" onFinish={onFinish}>
-                    <FieldGroup>
-                        <Form.Item
-                            name="first_name"
-                            rules={[{ required: true, message: "Please input your first name!" }]}
-                        >
-                            <Input placeholder="First Name" />
-                        </Form.Item>
-                        <Form.Item
-                            name="last_name"
-                            rules={[{ required: true, message: "Please input your last name!" }]}
-                        >
-                            <Input placeholder="Last Name" />
-                        </Form.Item>
-                    </FieldGroup>
-                    <FieldGroup>
-                        <Form.Item
-                            name="email"
-                            rules={[{ required: true, message: "Please input your email!" }]}
-                        >
-                            <Input placeholder="Email" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: "Please input your password!" }]}
-                        >
-                            <Input.Password placeholder="Password" />
-                        </Form.Item>
-                    </FieldGroup>
-                    <Form.Item>
+                    <Form.Item
+                        label="First Name"
+                        name="first_name"
+                        rules={[{ required: true, message: "Please input your first name!" }]}
+                    >
+                        <Input placeholder="First Name" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Last Name"
+                        name="last_name"
+                        rules={[{ required: true, message: "Please input your last name!" }]}
+                    >
+                        <Input placeholder="Last Name" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: "Please input your email!" }]}
+                    >
+                        <Input placeholder="Email" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phoneNumber"
+                        rules={[{ message: "Please input your Phone Number" }]}
+                    >
+                        <Input type="Number" placeholder="Phone Number" />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: "Please input your password!" }]}
+                    >
+                        <Input.Password placeholder="Password" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Confirm Password"
+                        name="confirmPassword"
+                        rules={[
+                            { required: true, message: 'Please confirm your password!' },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject(new Error('The two passwords do not match!'));
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password placeholder="Confirm Password" />
+                    </Form.Item>
+                    <Flex style={{
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        gap: '10px'
+                    }}>
                         <Button type="primary" htmlType="submit" block>
                             Sign Up
                         </Button>
-                    </Form.Item>
+                        <Button type="default" onClick={handleLoginClick} block>
+                            Already have an account? Login
+                        </Button>
+                    </Flex>
                 </Form>
-                <Button type="default" onClick={handleLoginClick} block>
-                    Already have an account? Login
-                </Button>
             </FormContainer>
         </Wrapper>
     );
