ec1cbea7691056f60cf7a648af7d86179f0eed9c
saadabid2223 <saadabid03@gmail.com>
2023-11-28 00:43:32 +0500

    email-confirmation-page

---
+++ src/features/AfterConfirmationEmail/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  paddingBottom: 100px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h3 {
+    color: #52c41a;
+  }
+`
+
+
