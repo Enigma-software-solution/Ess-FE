9467a6262db9ddd59b7369fba59c5599e38a8d8b
MAS3k <mian90kakakhel0@gmail.com>
2023-11-27 15:17:26 -0500

    loginform-minor-changes

--- src/features/ForgotPassword/index.js
+++ src/features/ForgotPassword/index.js
@@ -8,73 +8,68 @@ import { forgotPasswordApi, loginUser } from "src/store/slices/authSlice/apis";
 import { toast } from "react-toastify";
 import { routes } from "src/constant/routes";
 
-
 const ForgotPassword = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-
-    const onFinish = async (values) => {
-        try {
-            setIsLoading(true)
-            const res = await dispatch(forgotPasswordApi({ email: values.email, })).unwrap()
-            if (res) {
-                navigate("/");
-
-            }
-        } catch (err) {
-            toast.error(err?.message)
-        } finally {
-            setIsLoading(false)
-        }
-    };
-
-
-    const handleLogin = () => {
-        navigate(routes.LOGIN)
+  const onFinish = async (values) => {
+    try {
+      setIsLoading(true);
+      const res = await dispatch(
+        forgotPasswordApi({ email: values.email })
+      ).unwrap();
+      if (res) {
+        navigate("/");
+      }
+    } catch (err) {
+      toast.error(err?.message);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    return (
-        <Wrapper>
-            <FormContainer>
-                <Avatar
-                    size={64}
-                    icon={<UserOutlined />}
-                    style={{ marginBottom: 24 }}
-                />
-                <Title>Forgot Password</Title>
-                <Form name="forgot-password" onFinish={onFinish}>
-                    <Form.Item
-                        name="email"
-                        rules={[{ required: true, message: "Please input your email!" }]}
-                    >
-                        <Input placeholder="Email" />
-                    </Form.Item>
+  const handleLogin = () => {
+    navigate(routes.LOGIN);
+  };
 
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            block
-                            loading={isLoading}
-                        >
-                            Forgot password
-                        </Button>
-                    </Form.Item>
+  return (
+    <Wrapper>
+      <FormContainer>
+        <Avatar
+          size={64}
+          icon={<UserOutlined />}
+          style={{ marginBottom: 24 }}
+        />
+        <Title>Forgot Password</Title>
+        <Form name="forgot-password" onFinish={onFinish}>
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input size="large" placeholder="Email" />
+          </Form.Item>
 
-                    <Button type="link" onClick={handleLogin}>
-                        Login
-                    </Button>
-                </Form>
-                {/* <Button type="default" block onClick={handle}>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block loading={isLoading}>
+              Send Email
+            </Button>
+          </Form.Item>
+
+          <Button
+            style={{ float: "right", padding: "2px" }}
+            type="link"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+        </Form>
+        {/* <Button type="default" block onClick={handle}>
                     Don't have account? Signup
                 </Button> */}
-            </FormContainer>
-        </Wrapper>
-    );
+      </FormContainer>
+    </Wrapper>
+  );
 };
 
-
-
-export default ForgotPassword
+export default ForgotPassword;
