40aeda4d98184d84a4ea54e497f9244211a8869e
MAS3k <mian90kakakhel0@gmail.com>
2023-12-06 17:47:47 -0500

    policy-ui

---
+++ src/features/Policy/PolicySidebar/styled.js
@@ -0,0 +1,26 @@
+import { Flex } from 'antd';
+import styled from 'styled-components';
+
+export const Wrapper = styled(Flex)`
+    background-color: #fcfcfc;
+    height: 100vh;
+    padding: 30px;
+   line-height: 50px;
+justify-content: center;
+     h5 {
+        color:grey;
+        cursor: pointer;
+        &:hover {
+            color: #0C59DB;
+        }
+        line-height: 50px;
+        font-size: medium;
+       }
+.selected {
+        color: #0C59DB;
+    }
+    button{
+        text-align: center;
+        padding-left: 5px;
+    }`
+    ;
