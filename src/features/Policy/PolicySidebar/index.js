98ffae669a3b7f4f82304308983f05684cf1ee80
Phisher47 <ali.rza.work@gmail.com>
2023-12-08 21:37:01 +0500

    add action modal

--- src/features/Policy/PolicySidebar/index.js
+++ src/features/Policy/PolicySidebar/index.js
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Wrapper } from './styled';
 import { useNavigate } from 'react-router-dom';
-import { EditOutlined, LeftOutlined } from '@ant-design/icons';
-import { getAllPolicy, getSelectedPolicy } from 'src/store/slices/policySlice/selectors'
+import { EditOutlined, DeleteOutlined, LeftOutlined, MoreOutlined } from '@ant-design/icons';
+import { getAllPolicy, getSelectedPolicy } from 'src/store/slices/policySlice/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSelectedPolicy } from 'src/store/slices/policySlice';
-import { Button, Flex } from 'antd';
+import { Button, Flex, Dropdown, Menu } from 'antd';
 import PolicyModal from '../PolicyModal';
-
 
 const PolicySidebar = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const navigate = useNavigate();
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
-    const policyList = useSelector(getAllPolicy)
-    const selectedPolicy = useSelector(getSelectedPolicy)
+    const policyList = useSelector(getAllPolicy);
+    const selectedPolicy = useSelector(getSelectedPolicy);
 
     const handlePolicyClick = (item) => {
-        dispatch(setSelectedPolicy(item))
+        dispatch(setSelectedPolicy(item));
     };
 
     const handleEditClick = (item) => {
@@ -27,17 +26,38 @@ const PolicySidebar = () => {
         dispatch(setSelectedPolicy(item));
     };
 
-    useEffect(() => {
-        dispatch(setSelectedPolicy(policyList[0]))
-    }, [policyList])
+    const handleDeleteClick = (item) => {
 
+    };
+
+    const handleMenuClick = ({ key }, item) => {
+        if (key === 'edit') {
+            handleEditClick(item);
+        } else if (key === 'delete') {
+            handleDeleteClick(item);
+        }
+    };
+
+    const menu = (item) => (
+        <Menu onClick={(e) => handleMenuClick(e, item)}>
+            <Menu.Item key="edit">
+                <EditOutlined /> Edit
+            </Menu.Item>
+            <Menu.Item key="delete">
+                <DeleteOutlined /> Delete
+            </Menu.Item>
+        </Menu>
+    );
+
+    useEffect(() => {
+        dispatch(setSelectedPolicy(policyList[0]));
+    }, [policyList]);
 
     return (
         <Wrapper style={{ position: 'fixed', display: 'flex', flexDirection: 'column' }}>
             <LeftOutlined style={{ position: 'absolute', top: '20px', left: '20px', fontSize: '20px' }} onClick={() => navigate(-1)} />
             {policyList?.map((item, index) => (
                 <Flex justify='space-between' key={index} className={` ${selectedPolicy === item ? 'selected' : ''}`}>
-
                     <h5
                         key={index}
                         className={`m-1 ${selectedPolicy === item ? 'selected' : ''}`}
@@ -45,14 +65,14 @@ const PolicySidebar = () => {
                     >
                         {`${index + 1}. ${item?.title}`}
                     </h5>
-                    <Button onClick={() => handleEditClick(item)}> <EditOutlined /></Button>
+                    <Dropdown overlay={() => menu(item)} trigger={['click']}>
+                        <MoreOutlined style={{ fontSize: '20px', cursor: 'pointer' }} />
+                    </Dropdown>
                 </Flex>
-
-            ))
-            }
+            ))}
             {isModalOpen && <PolicyModal open={isModalOpen} handleClose={() => setIsModalOpen(false)} selectedPolicy={selectedPolicy} />}
-
-        </Wrapper >
+        </Wrapper>
     );
 };
+
 export default PolicySidebar;
