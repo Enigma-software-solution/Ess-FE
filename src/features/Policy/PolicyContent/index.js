de45630c35993c44062ed2224eac8489f85848cc
meetwaytech <meetway.tech@gmail.com>
2023-12-12 03:20:06 +0500

    hot fixes

--- src/features/Policy/PolicyContent/index.js
+++ src/features/Policy/PolicyContent/index.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { Wrapper } from "./styled";
 import { getSelectedPolicy } from "src/store/slices/policySlice/selectors";
 import { useSelector } from "react-redux";
-import { Flex } from "antd";
+import { Empty, Flex } from "antd";
 import { Watermark } from 'antd';
 import AddButton from "src/components/buttons/AddButton";
 import PolicyModal from "../PolicyModal";
 import { getLogedInUser } from "src/store/slices/authSlice/selectors";
 import { ROLES } from "src/constant/roles";
+import { isEmpty } from "lodash";
 
-const PolicyContent = () => {
+const PolicyContent = ({ policyList }) => {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const selectedPolicy = useSelector(getSelectedPolicy);
 
@@ -29,16 +30,24 @@ const PolicyContent = () => {
                         <AddButton text='Create new policy' onClick={() => setIsModalOpen(true)} />
                     }
                 </Flex>
-                <div style={{ flex: 1, padding: '0px 40px 50px', }}>
-                    <h2>Terms of Services</h2>
-                    <h4 style={{ textTransform: 'capitalize', padding: '12px 0px', marginTop: '10px' }}>{selectedPolicy?.title}</h4>
 
-                    <Watermark content="ENIGMA SOFTWARE SOLUTION" offset={[400, 300]} gap={[300, 550]}>
-                        <div
-                            dangerouslySetInnerHTML={{ __html: selectedPolicy?.content }}
-                        />
-                    </Watermark>
-                </div>
+                {
+                    isEmpty(policyList) ?
+                        <Flex justify='center' align='center' >
+                            <Empty />
+                        </Flex>
+                        : <div style={{ flex: 1, padding: '0px 40px 50px', }}>
+                            <h2>Terms of Services</h2>
+                            <h4 style={{ textTransform: 'capitalize', padding: '12px 0px', marginTop: '10px' }}>{selectedPolicy?.title}</h4>
+
+                            <Watermark content="ENIGMA SOFTWARE SOLUTION" offset={[400, 300]} gap={[300, 550]}>
+                                <div
+                                    dangerouslySetInnerHTML={{ __html: selectedPolicy?.content }}
+                                />
+                            </Watermark>
+                        </div>
+
+                }
             </Flex>
 
             <PolicyModal open={isModalOpen} handleClose={() => setIsModalOpen(false)} />
