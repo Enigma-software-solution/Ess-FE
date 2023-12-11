2d27ecf548a21217f03c4e23930fadee9b8ab1e0
Phisher47 <ali.rza.work@gmail.com>
2023-12-11 19:36:52 +0500

    empty added in policy

--- src/features/Policy/index.js
+++ src/features/Policy/index.js
@@ -1,14 +1,18 @@
-import { Flex, } from 'antd'
+import { Empty, Flex, } from 'antd'
 import React, { useEffect } from 'react'
 import PolicySidebar from './PolicySidebar'
 import PolicyContent from './PolicyContent'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPolicyApi } from 'src/store/slices/policySlice/apis'
-import { getLoading } from 'src/store/slices/policySlice/selectors'
+import { getAllPolicy, getLoading } from 'src/store/slices/policySlice/selectors'
 import Loader from 'src/components/Loader'
+import { isEmpty } from 'lodash'
 
 const Policy = () => {
     const dispatch = useDispatch()
+
+    const policyList = useSelector(getAllPolicy);
+
 
     const isLoading = useSelector(getLoading)
 
@@ -19,10 +23,17 @@ const Policy = () => {
 
     if (isLoading) return <Loader />
 
+    if (isEmpty(policyList)) {
+        return <Flex justify='center' align='center' style={{ height: '100vh' }}>
+            <Empty />
+        </Flex>
+    }
+
+
     return (
         <Flex>
             <div style={{ flex: 1 }}>
-                <PolicySidebar />
+                <PolicySidebar policyList={policyList} />
             </div>
 
             <div style={{ flex: 5 }}>
