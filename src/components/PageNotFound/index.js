54a252fbb5a55268b3c865d281c3f24446c64c6f
MAS3k <mian90kakakhel0@gmail.com>
2023-12-13 10:27:11 -0500

    navigate  issue solved

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Button, Result } from 'antd';
-const NotFound = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
-  />
-);
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  const goToDashboard = () => {
+    navigate('/dashboard/attendance/dashboard');
+  };
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button onClick={goToDashboard} type="primary">Back Home</Button>}
+    />
+  );
+};
 export default NotFound;
+
