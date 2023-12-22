ccdd505115dee6f643bafbf2b26654db9941a940
meetwaytech <meetway.tech@gmail.com>
2023-12-22 20:34:13 +0500

    fixes

--- src/features/AttendanceFeature/AttendanceSubmission/MarkCard/index.js
+++ src/features/AttendanceFeature/AttendanceSubmission/MarkCard/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextArea from 'antd/es/input/TextArea';
 import Avatar from 'src/assets/avatar1.jpg';
 import { CardImage, CardWrapper } from './styled';
 import { ImageWrapper } from '../styled';
 import Form from 'antd/es/form/Form';
 import format from 'date-fns/format';
-import { Button, Select } from 'antd';
+import { Button, Select, TimePicker } from 'antd';
 import { toast } from 'react-toastify';
 import { capitalize } from 'lodash';
+import { useForm } from 'antd/lib/form/Form';
+import { getTime } from 'date-fns';
+import dayjs from 'dayjs';
+
+const { Option } = Select;
 
 const STATUS_OPTIONS = [
     { value: 'present', label: 'Present' },
@@ -17,26 +22,33 @@ const STATUS_OPTIONS = [
     { value: 'half-day', label: 'Half-day' },
     { value: 'vacation', label: 'Vacation' },
 ];
+
 const MarkCard = ({ user, isLoading, handleSubmit, attendanceDate }) => {
-    const [form] = Form.useForm();
+    const [form] = useForm();
+    const [checkInTime, setCheckInTime] = useState(new Date());
 
     const onSubmit = async (values) => {
         if (!values.status) {
             return toast.warn('Status is required');
         }
         const date = format(attendanceDate, 'yyyy-MM-dd');
-        const attendanceTime = new Date();
+        const time = format(new Date(checkInTime), 'HH:mm:ss'); // Extract time from checkInTime
+        const combinedCheckInTime = new Date(`${date}T${time}`);
+
         const data = {
             user: user?._id,
             date: date,
             status: values?.status,
-            checkInTime: ['late', 'present', 'half-day'].includes(values?.status) ? attendanceTime : null,
+            checkInTime: ['late', 'present', 'half-day'].includes(values?.status)
+                ? combinedCheckInTime
+                : null,
             notes: values?.notes,
         };
+
         try {
             await handleSubmit(data);
         } catch (err) {
-            console.log(err);
+            console.error(err);
         }
     };
 
@@ -49,9 +61,20 @@ const MarkCard = ({ user, isLoading, handleSubmit, attendanceDate }) => {
                 {capitalize(user?.first_name) + ' ' + capitalize(user?.last_name)}
             </h5>
             <Form form={form} onFinish={onSubmit}>
-                <Form.Item name="status" >
+                <Form.Item name="checkInTime">
+                    <TimePicker
+                        style={{ width: '100%' }}
+                        placeholder="Select Time"
+                        use12Hours
+                        format="h:mm a"
+                        size="large"
+                        onChange={(time) => setCheckInTime(time)}
+                        defaultValue={dayjs(checkInTime)}
+                    />
+                </Form.Item>
+                <Form.Item name="status">
                     <Select
-                        placeholder='Select Status'
+                        placeholder="Select Status"
                         dropdownStyle={{ background: '#e4eefc', fontSize: '25px' }}
                         showSearch
                         size="large"
@@ -59,18 +82,16 @@ const MarkCard = ({ user, isLoading, handleSubmit, attendanceDate }) => {
                         rules={[{ required: true, message: 'Please select a status!' }]}
                     />
                 </Form.Item>
+
                 <Form.Item name="notes">
-                    <TextArea rows={3} placeholder='Notes' />
+                    <TextArea rows={3} placeholder="Notes" />
                 </Form.Item>
-                <Button
-                    htmlType='submit'
-                    type="primary"
-                    disabled={isLoading}
-                >
+                <Button htmlType="submit" type="primary" disabled={isLoading}>
                     Submit
                 </Button>
             </Form>
         </CardWrapper>
     );
 };
+
 export default MarkCard;
