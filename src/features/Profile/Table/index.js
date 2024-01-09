b0b22bc157de576fa0f8a5d1e334607c23b51065
MAS3k <mian90kakakhel0@gmail.com>
2024-01-09 11:01:16 -0500

    fe issue solved

--- src/features/Profile/Table/index.js
+++ src/features/Profile/Table/index.js
@@ -3,13 +3,13 @@ import { Popconfirm, Table } from 'antd';
 import EditButton from 'src/components/buttons/EditButton';
 import DeleteButton from 'src/components/buttons/DeleteButton';
 import { useDispatch, useSelector } from 'react-redux';
-import { deteleProfileApi, getProfilesApi } from "src/store/slices/profielSlice/apis";
+import { deteleProfileApi, getProfilesApi, updateProfileApi } from "src/store/slices/profielSlice/apis";
 import { getAllProfiles, isProfileLaoding } from "src/store/slices/profielSlice/selectors";
 import Header from "../Header";
 import { setSelectedProfile } from "src/store/slices/profielSlice";
 import CreateProfileDrawer from "../Drawers/CreateProfileDrawer";
 import Loader from "src/components/Loader";
-import { getPolicyApi } from "src/store/slices/policySlice/apis";
+import { StyledBadge } from "./styled";
 
 const ProfileTable = () => {
     const [isEditDrawerOpen, setIsEditDrawerOpen] = useState(false);
@@ -19,6 +19,8 @@ const ProfileTable = () => {
     const dispatch = useDispatch()
 
     const profileData = useSelector(getAllProfiles)
+
+    console.log(profileData, "testing")
 
     const handleEdit = (record, e) => {
         e.stopPropagation();
@@ -35,6 +37,22 @@ const ProfileTable = () => {
         e.stopPropagation();
         dispatch(deteleProfileApi(recordToDelete._id));
     };
+    const handleChangeStatus = (e, record) => {
+        e.stopPropagation();
+        if (record._id) {
+            dispatch(updateProfileApi({
+                id: record._id,
+                data: {
+                    ...record,
+                    status: record?.status === "active" ? 'inactive' : 'active'
+                }
+            }));
+        } else {
+            console.error("User record does not have a valid _id");
+        }
+    };
+
+
 
     const columns = [
         {
@@ -53,9 +71,27 @@ const ProfileTable = () => {
             dataIndex: "phoneNumber",
         },
         {
-            key: "status",
             title: "Status",
             dataIndex: "status",
+            render: (text, record) => {
+                const isActive = text === "active";
+
+
+                return (
+                    <div className="d-flex gap-1">
+                        <Popconfirm
+                            title={`Are you sure to ${isActive ? "deactivate" : "activate"} this Profile?`}
+                            onConfirm={(e) => handleChangeStatus(e, record)}
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <StyledBadge onClick={(e) => e.stopPropagation()} status={text}>
+                                {text}
+                            </StyledBadge>
+                        </Popconfirm>
+                    </div >
+                );
+            }
         },
         {
             key: "action",
