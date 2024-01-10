ae6cb7a08f484f8f74c4c59e6425f08883940985
MAS3k <mian90kakakhel0@gmail.com>
2024-01-10 10:32:43 -0500

    color issue solved

--- src/constant/colors.js
+++ src/constant/colors.js
@@ -1,7 +1,7 @@
 export const AttendanceStatusColor = {
-    Present: "#52c41a",
-    Absent: "#f5222d",
-    Leave: "#1677ff",
+    Present: "#1677ff",
+    Absent: "#C21807",
+    Leave: "#f5222d",
     Vacation: "#fadb14",
     Late: "#fa8c16",
     HalfDay: "#8a2be2",
