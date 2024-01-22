a36fd633861e28b045b4d8678b356c6ebc7878f4
Hassan <hassan.ali6085@gmail.com>
2024-01-23 02:57:53 +0500

    Users-table-pagination-fixed

--- src/store/slices/userSlice/selectors.js
+++ src/store/slices/userSlice/selectors.js
@@ -1,3 +1,4 @@
 export const getAllUsers = (state) => state?.users?.data?.Users
+export const getAllUsersPagination = (state) => state?.users?.data?.paginator
 export const getSelectedUser = (state) => state?.users?.selectedUser
 export const isUserLoading = (state) => state?.users?.loading
