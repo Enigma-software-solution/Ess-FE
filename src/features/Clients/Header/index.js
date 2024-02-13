3ec785171660bb9b6bc160e7f5b039086fa71a0d
Hassan <hassan.ali6085@gmail.com>
2024-02-13 20:48:31 +0500

    search-added

--- src/features/Clients/Header/index.js
+++ src/features/Clients/Header/index.js
@@ -3,8 +3,9 @@ import AddButton from "src/components/buttons/AddButton";
 import CreateClientDrawer from "../CreateClientDrawer";
 import { useDispatch } from "react-redux";
 import { setSelectedClient } from "src/store/slices/clientSlice";
+import CustomSearchField from "src/components/SearchField";
 
-const Header = ({isOpen ,setIsOpen}) => {
+const Header = ({isOpen ,setIsOpen,onSearch}) => {
 
     const dispatch = useDispatch()
 
@@ -15,9 +16,15 @@ const Header = ({isOpen ,setIsOpen}) => {
 
     };
 
+    const handleSearchChange = (e) => {
+        const searchValue = e.target.value;
+        onSearch(searchValue);
+    };
+
     return (
         <div>
-            <div className="d-flex flex-row-reverse mb-3">
+            <div className="d-flex justify-content-between mb-3">
+                <CustomSearchField onChange={handleSearchChange} text="Search Client" />
                 <AddButton onClick={handleDrawer} text="New Client" />
             </div>
             <CreateClientDrawer isOpen={isOpen} handleDrawer={handleDrawer} />
