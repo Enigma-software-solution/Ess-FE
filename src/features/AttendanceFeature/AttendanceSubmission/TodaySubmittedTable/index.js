5b53454ee4b1a77c9155261cdce11e68139ef7e9
talha-upbrainery <talha.dev@upbrainery.com>
2024-08-23 20:41:43 +0500

    timezone issue fixed globally

--- src/features/AttendanceFeature/AttendanceSubmission/TodaySubmittedTable/index.js
+++ src/features/AttendanceFeature/AttendanceSubmission/TodaySubmittedTable/index.js
@@ -11,112 +11,129 @@ import EditAttendanceModal from "../EditAttendanceModal";
 import { capitalize } from "lodash";
 
 const TodaySubmittedTable = ({ todayAllAttendance }) => {
-    const [isEditModalVisible, setIsEditModalVisible] = useState(false)
-    const dispatch = useDispatch();
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+  const dispatch = useDispatch();
 
+  const handleConfirmDelete = (recordToDelete, e) => {
+    e.stopPropagation();
+    dispatch(deleteAttendance(recordToDelete._id));
+  };
 
-    const handleConfirmDelete = (recordToDelete, e) => {
-        e.stopPropagation();
-        dispatch(deleteAttendance(recordToDelete._id));
-    };
+  const handleEdit = (record, e) => {
+    e.stopPropagation();
+    dispatch(setSelectedAttendance(record));
+    setIsEditModalVisible(true);
+  };
 
-    const handleEdit = (record, e) => {
-        e.stopPropagation();
-        dispatch(setSelectedAttendance(record));
-        setIsEditModalVisible(true);
-    };
+  const handleModalClose = () => {
+    setIsEditModalVisible(false);
+    dispatch(setSelectedAttendance(null));
+  };
 
-    const handleModalClose = () => {
-        setIsEditModalVisible(false)
-        dispatch(setSelectedAttendance(null))
-    }
+  const columns = [
+    {
+      title: "User Name",
+      dataIndex: "user?.first_name",
+      key: "first_name",
+      render: (text, record) =>
+        capitalize(record?.user?.first_name) +
+        " " +
+        capitalize(record?.user?.last_name),
+    },
 
-    const columns = [
-        {
-            title: "User Name",
-            dataIndex: "user?.first_name",
-            key: "first_name",
-            render: (text, record) => capitalize(record?.user?.first_name) + ' ' + capitalize(record?.user?.last_name),
-        },
+    {
+      title: "Check In Time",
+      dataIndex: "checkInTime",
+      render: (text, record) =>
+        record?.checkInTime ? format(new Date(record.checkInTime), "p") : "N/A",
+    },
+    {
+      title: "Check Out Time",
+      dataIndex: "checkOutTime",
+      render: (text, record) =>
+        record?.checkOutTime
+          ? format(new Date(record?.checkOutTime), "p")
+          : "N/A",
+    },
 
-        {
-            title: "Check In Time",
-            dataIndex: "checkInTime",
-            render: (text, record) => record?.checkInTime ? format(new Date(record.checkInTime), "p") : 'N/A',
-        },
-        {
-            title: "Check Out Time",
-            dataIndex: "checkOutTime",
-            render: (text, record) => record?.checkOutTime ? format(new Date(record?.checkOutTime), "p") : 'N/A',
-        },
+    {
+      title: "Notes",
+      dataIndex: "notes",
+      ellipsis: true,
+      render: (text, record) => (
+        <Tooltip
+          title={capitalize(text)}
+          placement="topLeft"
+          arrowPointAtCenter
+        >
+          <div
+            style={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            {capitalize(text)}
+          </div>
+        </Tooltip>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (text) => {
+        const tagStyle = {
+          width: "60px",
+          display: "inline-block",
+          textAlign: "center",
+        };
 
-        {
-            title: 'Notes',
-            dataIndex: 'notes',
-            ellipsis: true,
-            render: (text, record) => (
-                <Tooltip title={capitalize(text)} placement="topLeft" arrowPointAtCenter>
-                    <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                        {capitalize(text)}
-                    </div>
-                </Tooltip>
-            ),
-        },
-        {
-            title: "Status",
-            dataIndex: "status",
-            render: (text) => {
-                const tagStyle = {
-                    width: '60px',
-                    display: 'inline-block',
-                    textAlign: 'center',
-                };
+        return (
+          <Tag color={CheckAttendanceStatusColor(text)} style={tagStyle}>
+            {capitalize(text)}
+          </Tag>
+        );
+      },
+    },
 
-                return (
-                    <Tag color={CheckAttendanceStatusColor(text)} style={tagStyle}>
-                        {capitalize(text)}
-                    </Tag>
-                );
-            },
-        },
-
-        {
-            key: "action",
-            title: "Action",
-            dataIndex: "action",
-            render: (text, record) => (
-                <div className="d-flex gap-1">
-                    <EditButton onClick={(e) => handleEdit(record, e)} />
-                    <Popconfirm
-                        title="Are you sure you want to delete
+    {
+      key: "action",
+      title: "Action",
+      dataIndex: "action",
+      render: (text, record) => (
+        <div className="d-flex gap-1">
+          <EditButton onClick={(e) => handleEdit(record, e)} />
+          <Popconfirm
+            title="Are you sure you want to delete
                                    this user attendance?"
-                        onConfirm={(e) => handleConfirmDelete(record, e)}
-                        onCancel={(e) => e.stopPropagation()}
-                        okText="Yes"
-                        cancelText="No"
-                    >
-                        <DeleteButton onClick={(e) => e.stopPropagation()}>Delete</DeleteButton>
-                    </Popconfirm>
-                </div>
-            ),
-        },
-    ];
-
-    return (
-        <div>
-
-            <Table
-                dataSource={todayAllAttendance?.attendance}
-                columns={columns}
-                pagination={false}
-            />
-
-            <EditAttendanceModal
-                visible={isEditModalVisible}
-                onClose={handleModalClose}
-            />
+            onConfirm={(e) => handleConfirmDelete(record, e)}
+            onCancel={(e) => e.stopPropagation()}
+            okText="Yes"
+            cancelText="No"
+          >
+            <DeleteButton onClick={(e) => e.stopPropagation()}>
+              Delete
+            </DeleteButton>
+          </Popconfirm>
         </div>
-    )
-}
+      ),
+    },
+  ];
 
-export default TodaySubmittedTable
+  return (
+    <div>
+      <Table
+        dataSource={todayAllAttendance?.attendance}
+        columns={columns}
+        pagination={false}
+      />
+
+      <EditAttendanceModal
+        visible={isEditModalVisible}
+        onClose={handleModalClose}
+      />
+    </div>
+  );
+};
+
+export default TodaySubmittedTable;
--- src/utils/formatsOfDate.js
+++ src/utils/formatsOfDate.js
@@ -1,10 +1,23 @@
-import { format } from 'date-fns';
+// import { format } from 'date-fns';
+
+// export function formatDate(date, defaultFormat = "dd-MM-yyyy") {
+//     return format(new Date(date), defaultFormat);
+// }
+
+// export function formatTime(date, defaultFormat = "p") {
+//     return format(new Date(date), defaultFormat);
+// }
+import { format } from "date-fns";
+import { utcToZonedTime, format as formatInTimeZone } from "date-fns-tz";
+
+const timeZone = "America/New_York"; // EST Timezone
 
 export function formatDate(date, defaultFormat = "dd-MM-yyyy") {
-    return format(new Date(date), defaultFormat);
+  const zonedDate = utcToZonedTime(new Date(date), timeZone);
+  return format(zonedDate, defaultFormat);
 }
 
-
 export function formatTime(date, defaultFormat = "p") {
-    return format(new Date(date), defaultFormat);
+  const zonedDate = utcToZonedTime(new Date(date), timeZone);
+  return format(zonedDate, defaultFormat);
 }
